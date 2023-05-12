6f6d34d705ed0160624a2bab9c5501c5547ddc8c
Пользователь <@DESKTOP-KVSP5S9>
2023-05-12 09:40:48 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using Task1._2.View;
+
+namespace Task1._2
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            new LoginWindow().Show();
+        }
+    }
+}
